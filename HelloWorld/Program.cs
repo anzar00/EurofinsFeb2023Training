8e5e6b7f3cc8af2d6657bbe2b99708d9def2f987
Admin <Admin@DESKTOP-EPM9O6Q>
2023-02-06 16:03:38 +0530

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        public string PersonName;
+        static void Main(string[] args)
+        {
+            string personName = "csfvfvf";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
