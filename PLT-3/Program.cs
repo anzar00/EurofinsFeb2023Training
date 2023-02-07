bcea00fd20c33019460c62f87120b4510ec8b162
anzar000 <peerzada.anzar@gmail.com>
2023-02-07 22:22:43 +0530

    PLT-3

---
+++ PLT-3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLT_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a pseudocode to accept a number and display whether it is an even or
+            //odd number
+
+            Console.WriteLine("Enter the number :");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 2 == 0)
+                Console.WriteLine("The number is even");
+            else
+                Console.WriteLine("The number is odd");
+
+            Console.ReadLine();
+        }
+    }
+}
