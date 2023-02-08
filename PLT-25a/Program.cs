c8c96e9a0a956589f0731096c72f2924be11c026
anzar000 <peerzada.anzar@gmail.com>
2023-02-08 21:20:08 +0530

    PLT-25a

--- PLT-25a/Program.cs
+++ PLT-25a/Program.cs
@@ -20,23 +20,43 @@ namespace PLT_25a
             //: 
             //N rows
 
-            Console.WriteLine("Enter N: ");
-            int n = Convert.ToInt32(Console.ReadLine());
 
-            int number = 1;
-            for (int i = 1; i <= n; i++)
+            //Console.WriteLine("Enter N: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //int number = 1;
+            //for (int i = 1; i <= n; i++)
+            //{
+            //    for (int j = 1; j <= i; j++)
+            //    {
+            //        if (j==1 & i==1) Console.Write(number);
+            //        else
+            //            Console.Write((number * number) * (j % 2 != 0 ? -1 : 1) + " ");
+            //        number++;
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //Console.ReadLine();
+
+            int i, j, n = 0;
+            int a = 1;
+            int sign = 1;
+
+            Console.WriteLine("Enter N: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 1; i <= n; i++)
             {
-                for (int j = 1; j <= i; j++)
+                for (j = 1; j <= i; j++)
                 {
-                    if (j==1 & i==1) Console.Write(number);
-                    else
-                        Console.Write((number * number) * (j % 2 != 0 ? -1 : 1) + " ");
-                    number++;
+                    Console.Write(a * a * sign + " ");
+                    a = a + 1;
+                    sign = sign * (-1);
                 }
                 Console.WriteLine();
             }
-
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
