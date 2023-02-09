52c01b9ed261b1fc48140906e4bbb0e810190221
anzar000 <peerzada.anzar@gmail.com>
2023-02-09 09:52:42 +0530

    PLT-25

--- PLT-25a/Program.cs
+++ PLT-25a/Program.cs
@@ -5,7 +5,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PLT_25a
+namespace PLT_25
 {
     internal class Program
     {
@@ -19,30 +19,13 @@ namespace PLT_25a
             //: 
             //: 
             //N rows
-
-
-            //Console.WriteLine("Enter N: ");
-            //int n = Convert.ToInt32(Console.ReadLine());
-
-            //int number = 1;
-            //for (int i = 1; i <= n; i++)
-            //{
-            //    for (int j = 1; j <= i; j++)
-            //    {
-            //        if (j==1 & i==1) Console.Write(number);
-            //        else
-            //            Console.Write((number * number) * (j % 2 != 0 ? -1 : 1) + " ");
-            //        number++;
-            //    }
-            //    Console.WriteLine();
-            //}
-
-            //Console.ReadLine();
-
             int i, j, n = 0;
             int a = 1;
             int sign = 1;
-
+            Console.Write("Given pattern:\n" +
+                "1\n" +
+                "-4 9\n" +
+                "-16 25 -36 \n");
             Console.WriteLine("Enter N: ");
             n = Convert.ToInt32(Console.ReadLine());
 
