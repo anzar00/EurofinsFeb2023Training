d9875eba016e78b63d9833024b7a180389e35307
anzar000 <peerzada.anzar@gmail.com>
2023-02-13 02:35:56 +0530

    PLT-15, PLT-16

---
+++ PLT-15/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLT_15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a pseudocode to accept a decimal number. Display it in the binary form. 
+
+            Console.WriteLine("Enter a decimal number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int[] binary = new int[100];
+            int i = 0;
+
+            while (num > 0)
+            {
+                binary[i] = num % 2;
+                num /= 2;
+                i++;
+            }
+
+            Console.Write("The binary form of the number is: ");
+            for (int j = i - 1; j >= 0; j--)
+            {
+                Console.Write(binary[j]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PLT-16/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLT_16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a pseudocode to accept a binary number and display it in the decimal 
+            // form.
+
+            Console.WriteLine("Enter a binary number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int decimalNum = 0;
+            int baseNum = 1;
+            int temp = num;
+
+            while (temp > 0)
+            {
+                int lastDigit = temp % 10;
+                temp /= 10;
+
+                decimalNum += lastDigit * baseNum;
+                baseNum *= 2;
+            }
+
+            Console.WriteLine("The decimal form of the number is: " + decimalNum);
+
+            Console.ReadKey();
+        }
+    }
+}
