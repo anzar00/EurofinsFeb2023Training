f583d373134d517e850736bd7315975c2d27a009
anzar000 <peerzada.anzar@gmail.com>
2023-02-13 00:13:34 +0530

    PLT-11, PLT-28

---
+++ PLT-11/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLT_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a pseudocode to display a number in words.
+            //Ex. 270176
+            //Output: Two Seven Zero One Seven Six
+
+            int n = 0;
+            int digit = 0;
+            int i = 0;
+            int[] digits = new int[10];
+
+            Console.WriteLine("Enter N: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            while (n != 0)
+            {
+                digit = n % 10;
+                digits[i] = digit;
+                n = n / 10;
+                i++;
+            }
+
+            for (int j = i - 1; j >= 0; j--)
+            {
+                switch (digits[j])
+                {
+                    case 0:
+                        Console.Write("Zero ");
+                        break;
+                    case 1:
+                        Console.Write("One ");
+                        break;
+                    case 2:
+                        Console.Write("Two ");
+                        break;
+                    case 3:
+                        Console.Write("Three ");
+                        break;
+                    case 4:
+                        Console.Write("Four ");
+                        break;
+                    case 5:
+                        Console.Write("Five ");
+                        break;
+                    case 6:
+                        Console.Write("Six ");
+                        break;
+                    case 7:
+                        Console.Write("Seven ");
+                        break;
+                    case 8:
+                        Console.Write("Eight ");
+                        break;
+                    case 9:
+                        Console.Write("Nine ");
+                        break;
+                }
+            }
+
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ PLT-28/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLT_28
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a pseudocode to store elements into a M * N matrix of integer. Display the 
+            //matrix and its transpose.
+
+            Console.WriteLine("Enter M: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter N: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[,] matrix = new int[m, n];
+
+            Console.WriteLine("Enter the elements of the matrix: ");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("The matrix is: ");
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("The transpose of the matrix is: ");
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(matrix[j, i] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
