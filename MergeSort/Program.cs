cb84bc7a864d2f4bff42e6462098c588f3edea26
anzar000 <peerzada.anzar@gmail.com>
2023-02-12 23:18:13 +0530

    PLT-8

---
+++ MergeSort/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeSort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n = 0;
+
+            Console.WriteLine("Enter N: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            
+            int[] arr = new int[n];
+            
+            Console.WriteLine("Enter array elements: ");
+            for (i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            MergeSort(arr, 0, n - 1);
+
+            Console.WriteLine("Sorted array: ");
+
+            for (i = 0; i < n; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+
+            Console.ReadKey();
+        }
+
+        static void MergeSort(int[] arr, int l, int r)
+        {
+            if (l < r)
+            {
+                int m = (l + r) / 2;
+
+                MergeSort(arr, l, m);
+                MergeSort(arr, m + 1, r);
+
+                Merge(arr, l, m, r);
+            }
+        }
+
+        static void Merge(int[] arr, int l, int m, int r)
+        {
+            int i, j, k;
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            int[] L = new int[100];
+            int[] R = new int[100];
+
+            for (i = 0; i < n1; i++)
+            {
+                L[i] = arr[l + i];
+            }
+
+            for (j = 0; j < n2; j++)
+            {
+                R[j] = arr[m + 1 + j];
+            }
+
+            i = 0;
+            j = 0;
+            k = l;
+
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+    }
+}
---
+++ PLT-8/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Resources;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLT_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //A vendor offers software services to a client. Each resource is billed at some dollar rate
+            //per hour.The total cost of the project for the client is therefore, the total number of
+            //hours contributed by all the vendor resources * the dollar rate / hour.There are however
+            //some variants.
+            //The vendor might have purchased hardware/infrastructure or software licenses
+            //needed for the project. 
+            //The vendor might have utilized external consultants for the project. 
+            //The client looks at the vendor as a one-stop solution and hence external resources
+            //employed by the vendor need to be paid by the vendor.
+            //It might however be possible that the vendor’s hardware and software
+            //purchases are borne by the client.In this case, the client pays the vendor
+            //30% of the hardware/infrastructure costs.In case of software licenses, the
+            //client pays the vendor 50% of the cost, if they are commonly available and
+            //used, or 100% if the software is infrequently used or is proprietary client
+            //technology.
+            //The external consultants employed by the vendor will come at a dollar rate per
+            //hour.
+            //Accept the suitable inputs and display the profits / loss realized by the vendor.
+
+            double totalHours, ratePerHour = 0.0;
+            
+            char hasExtCon = ' ';
+            double extConRate = 0.0;
+            double extConHours = 0.0;
+
+            char hasHardware = ' ';
+            double hardwareCost = 0.0;
+
+            char hasSoftwareLic = ' ';
+            double softwareLicCost = 0.0;
+
+            char isCommonlyUsed = ' ';
+
+            double projectCost = 0.0;
+            double softwareCost = 0.0;
+
+            double profit = 0.0;
+
+            Console.WriteLine("Enter total hours: ");
+            totalHours = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter rate per hour: ");
+            ratePerHour = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Does the vendor have external consultants? (Y/N)");
+            hasExtCon = Convert.ToChar(Console.ReadLine());
+
+            if (hasExtCon == 'Y' || hasExtCon == 'y')
+            {
+                Console.WriteLine("Enter external consultant rate per hour: ");
+                extConRate = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter external consultant hours: ");
+                extConHours = Convert.ToDouble(Console.ReadLine());
+            }
+
+            Console.WriteLine("Does the project requie hardware? (Y/N)");
+            hasHardware = Convert.ToChar(Console.ReadLine());
+
+            if (hasHardware == 'Y' || hasHardware == 'y')
+            {
+                Console.WriteLine("Enter hardware cost: ");
+                hardwareCost = Convert.ToDouble(Console.ReadLine());
+            }
+
+            Console.WriteLine("Does the project require software licenses? (Y/N)");
+            hasSoftwareLic = Convert.ToChar(Console.ReadLine());
+
+            if (hasSoftwareLic == 'Y' || hasSoftwareLic == 'y')
+            {
+                Console.WriteLine("Enter software license cost: ");
+                softwareLicCost = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Is the software commonly used? (Y/N)");
+                isCommonlyUsed = Convert.ToChar(Console.ReadLine());
+            }
+
+            projectCost = (totalHours * ratePerHour) + (hardwareCost * .3);
+
+            if (isCommonlyUsed == 'Y' || isCommonlyUsed == 'y')
+            {
+                softwareCost = softwareLicCost * .5;
+            }
+            else
+            {
+                softwareCost = softwareLicCost;
+            }
+
+            projectCost += softwareCost;
+
+            Console.WriteLine("Project cost: {0}", projectCost);
+
+            profit = projectCost - (extConHours * extConRate) + (hardwareCost) + (softwareLicCost);
+
+            Console.WriteLine("Profit: {0}", profit);
+
+            if (profit > 0)
+            {
+                Console.WriteLine("Vendor made a profit of {0}", profit);
+            }
+            else if (profit < 0)
+            {
+                Console.WriteLine("Vendor made a loss of {0}", profit);
+            }
+            else
+            {
+                Console.WriteLine("Vendor broke even.");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
