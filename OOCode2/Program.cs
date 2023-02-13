b9647faa73bbb23c609b6d8b10c2cb773dc67ce9
anzar000 <peerzada.anzar@gmail.com>
2023-02-13 15:37:25 +0530

    Stack and Heap Memory allocation

---
+++ OOCode2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOCode2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            M1();
+            M1();
+        }
+
+        public static void M1()
+        {
+            int a;
+            a = 10;
+            string str;
+            str = "Hello World!";
+        }
+    }
+}
