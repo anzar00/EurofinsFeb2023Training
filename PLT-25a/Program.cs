bb4b457142f98eef84dd155415fccd9255ca5d09
anzar000 <peerzada.anzar@gmail.com>
2023-02-16 16:49:39 +0530

    PLT-25

--- PLT-25a/Program.cs
+++ PLT-25a/Program.cs
@@ -19,26 +19,119 @@ namespace PLT_25
             //: 
             //: 
             //N rows
-            int i, j, n = 0;
-            int a = 1;
-            int sign = 1;
+            //int i, j, n = 0;
+            //int a = 1;
+            //int sign = 1;
+            //Console.Write("Given pattern:\n" +
+            //    "1\n" +
+            //    "-4 9\n" +
+            //    "-16 25 -36 \n");
+            //Console.WriteLine("Enter N: ");
+            //n = Convert.ToInt32(Console.ReadLine());
+
+            //for (i = 1; i <= n; i++)
+            //{
+            //    for (j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(a * a * sign + " ");
+            //        a = a + 1;
+            //        sign = sign * (-1);
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //1
+            //1 2
+            //6 24 120
+            //: 
+            //: 
+            //N rows
+
+            //int i, j;
+            //int a = 1;
+            //int b = 0;
+            //Console.Write("Given pattern:\n" +
+            //    "1\n" +
+            //    "1 2\n" +
+            //    "6 24 120 \n");
+
+            //Console.WriteLine("Enter N: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //for (i = 1; i <= n; i++)
+            //{
+            //    for (j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(a + " ");
+            //        b = b + 1;
+            //        a = a * b;
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //   *
+            //  ** 
+            // ***
+            //****
+            //: 
+            //N rows
+
+            //int i, j = 0;
+            //Console.Write("Given pattern:\n" +
+            //    "      *\n" +
+            //    "    * *\n" +
+            //    "  * * *\n" +
+            //    "* * * *\n");
+
+            //Console.WriteLine("Enter N: ");
+            //int n = Convert.ToInt32(Console.ReadLine());
+
+            //for (i = 1; i <= n; i++)
+            //{
+            //    for (j = 1; j <= n-i; j++)
+            //    {
+            //        Console.Write("  ");
+            //    }
+
+            //    for (j = 1; j <= i; j++)
+            //    {
+            //        Console.Write(" *");
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            //   *
+            //  ***
+            // *****
+            //*******
+            //: 
+            //N rows
+
+            int i, j = 0;
             Console.Write("Given pattern:\n" +
-                "1\n" +
-                "-4 9\n" +
-                "-16 25 -36 \n");
+                "   *\n" +
+                "  * * *\n" +
+                " * * * * *\n" +
+                "* * * * * * *\n");
+
             Console.WriteLine("Enter N: ");
-            n = Convert.ToInt32(Console.ReadLine());
+            int n = Convert.ToInt32(Console.ReadLine());
 
             for (i = 1; i <= n; i++)
             {
-                for (j = 1; j <= i; j++)
+                for (j = 1; j <= n-i; j++)
                 {
-                    Console.Write(a * a * sign + " ");
-                    a = a + 1;
-                    sign = sign * (-1);
+                    Console.Write("  ");
+                }
+
+                for (j = 1; j <= 2*i-1; j++)
+                {
+                    Console.Write(" *");
                 }
                 Console.WriteLine();
             }
+
+
             Console.ReadKey();
         }
     }
