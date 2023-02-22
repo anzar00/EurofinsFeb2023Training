34922cb32ae39a128a4acf139055661f6e672f1f
anzar000 <peerzada.anzar@gmail.com>
2023-02-22 11:46:02 +0530

    Simple Calculator - Unit Test

---
+++ SimpleCalculator.UnitTest/Calculator.UnitTest.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimpleCalculatorLibrary;
+using System;
+
+namespace SimpleCalculator.UnitTest
+{
+    [TestClass]
+    public class CalculatorUnitTest
+    {
+        [TestMethod]
+        public void Sum_WithValidInput_ShouldGiveValidResult() //Test Casees
+        {
+            // do not use contitional statemetns
+            // do not use iterative statements
+            // do not use try catch blocks
+            // write only simple statements
+
+            // AAA - Arrange, Act, Assert approach
+
+            // Arrange
+            int a = 10;
+            int b = 20;
+            int expectedResult = 30;
+            Calculator calculator = new Calculator();
+
+            // Act
+            int actual = calculator.Sum(a, b);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(NegativeInputException))]
+        public void Sum_WithNegativeInput_ShouldThrowExceptiont() //Test Casees
+        {
+            // do not use contitional statemetns
+            // do not use iterative statements
+            // do not use try catch blocks
+            // write only simple statements
+
+            // AAA - Arrange, Act, Assert approach
+
+            Calculator calculator = new Calculator();
+            calculator.Sum(-1, -1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(OddInputException))]
+        public void Sum_WithOddInput_ShouldThrowExceptiont() //Test Casees
+        {
+            // do not use contitional statemetns
+            // do not use iterative statements
+            // do not use try catch blocks
+            // write only simple statements
+
+            // AAA - Arrange, Act, Assert approach
+            new Calculator().Sum(1, 1);
+        }
+    }
+}
+  
---
+++ SimpleCalculator.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SimpleCalculator.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimpleCalculator.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("030f6544-fb55-4b05-ac6c-7b166e60f494")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
