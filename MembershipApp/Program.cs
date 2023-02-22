e6ba82ad093bfcf3b2e9695d470eefd5241a9477
anzar000 <peerzada.anzar@gmail.com>
2023-02-22 11:45:11 +0530

    Membership App

---
+++ MembershipApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembershipApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Company
+    {
+        
+    }
+
+    class Customer
+    {
+
+    }
+
+    class RegCustomer : Customer
+    { 
+    
+    }
+
+    class MembershipFactory
+    {
+
+    }
+
+    class Membership
+    { 
+    
+    }
+
+
+}
