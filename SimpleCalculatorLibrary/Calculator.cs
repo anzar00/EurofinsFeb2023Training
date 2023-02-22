8fff6cc2b593a764bba4c985aee001ef8256e50d
anzar000 <peerzada.anzar@gmail.com>
2023-02-22 11:46:38 +0530

    Simple Calculator - BL for Unit Testing

---
+++ SimpleCalculatorLibrary/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculatorLibrary
+{
+    public class Calculator
+    {
+        // Find the sum og two ints and return the result
+        // non negative numbers only
+        // even numbers only
+        // throws suitable expresion if business rule is violated
+        public int Sum(int a, int b)
+        {
+            if(a < 0 || b < 0)
+                throw new NegativeInputException("Negative numbers are not allowed");
+            if(a % 2 != 0 || b % 2 != 0)
+                throw new OddInputException("Odd numbers are not allowed");
+            return a + b;
+        }
+    }
+}
---
+++ SimpleCalculatorLibrary/OddInputException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculatorLibrary
+{
+    public class OddInputException : ApplicationException
+    {
+        public OddInputException(string message = null, Exception inner = null) : base(message, inner)
+        {
+
+        }
+    }
+}
