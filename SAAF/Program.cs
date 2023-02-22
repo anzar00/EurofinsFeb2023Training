e71ec6488ad360fbd80f83323029fc0914d9cdf2
anzar000 <peerzada.anzar@gmail.com>
2023-02-22 11:45:39 +0530

    SAAF

--- SAAF/Program.cs
+++ SAAF/Program.cs
@@ -10,6 +10,43 @@ namespace SAAF
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            Hospital hospital = new Hospital();
+            Console.WriteLine("Created Hospital");
+            int n = random.Next(15, 20);
+            Console.WriteLine($"Creating {n} Wards");
+            WardType[] types = { WardType.IntensiveCare, WardType.GeneralUnit, WardType.PediatricUnit, WardType.SurgicalUnit };
+            for (int i = 0; i < n; i++)
+            {
+                Ward ward = new Ward()
+                {
+                    WardName = $"Ward{i}",
+                    WardType = types[random.Next(types.Length)],
+                    BasicCost = random.NextDouble()*5000+5000
+                };
+                int n2 = random.Next(20, 25);
+                Console.WriteLine($"Creating {n2} Patients");
+                for (int j = 0; j<n2; j++)
+                {
+                    ward.Patients.Add(new Patient());
+                }
+                hospital.Wards.Add(ward);
+            }
+            n = random.Next(135, 150);
+            Console.WriteLine($"Creating {n} Doctors");
+            for (int i = 0; i < n; i++)
+            {
+                double roulete = random.NextDouble();
+                Employee emp = new Employee();
+                if (roulete < 0.1) emp = new ConsultantDoctor();
+                else if (roulete < 0.2) emp = new InternalDoctor();
+                else if (roulete < 0.8) emp = new Doctor() { Speciality = $"S{random.Next(5, 9)}" };
+                else if (roulete < 0.9) emp = new Receptionist();
+                else emp = new Nurse();
+                hospital.Employees.Add(emp);
+            }
+            Console.WriteLine(hospital.GetTotalPatients());
+            Console.WriteLine(hospital.GetTotalDoctors());
         }
     }
 
@@ -53,7 +90,7 @@ namespace SAAF
 
         }
 
-    class Receptionsit : Employee
+    class Receptionist : Employee
         {
 
         }
@@ -129,23 +166,84 @@ namespace SAAF
         //Hospital has many Wards
         public List<Ward> Wards { get; set; } = new List<Ward>();
 
-       //Get Total Patients count.
-       
+        //Get Total Patients count.
+
+        public int GetTotalPatients()
+        {
+            int count = 0;
+            foreach (var ward in Wards)
+            {
+                count += ward.Patients.Count;
+            }
+            return count;
+        }
+
+        //Get Total Patients by Ward.
+
+        public int GetTotalPatientsByWard(WardType wardType)
+        {
+            int count = 0;
+            foreach (var ward in Wards)
+            {
+                if (ward.WardType == wardType)
+                {
+                    count += ward.Patients.Count;
+                }
+            }
+            return count;
+        }
+
+        //Get Total Doctors count. 
+
+        public int GetTotalDoctors()
+        {
+            int count = 0;
+            foreach (var employee in Employees)
+            {
+                if (employee is Doctor)
+                    count++;
+            }
+            return count;
+        }
+
+        //Get Doctors by specialization.
+
+        public int GetDoctorsBySpecialization(string Speciality)
+        {
+            int count = 0;
+            foreach(var employee in Employees)
+            {
+                if (employee is Doctor)
+                    if((employee as Doctor).Speciality == Speciality)
+                        count++;
+            }
+            return count;
+        }
+
+        //Get Total Junior Doctors count.
         
+        public int GetTotalInternalDoctors()
+        {
+            int count = 0;
+            foreach (var employee in Employees)
+            {
+                if (employee is InternalDoctor)
+                    count++;
+            }
+            return count;
+        }
 
-
-
-       //Get Total Patients by Ward.
-
-       //Get Total Doctors count. 
-
-       //Get Doctors by specialization.
-
-       //Get Total Junior Doctors count.
-
-       //Get Ward Cost by Type.
-
-
+        //Get Ward Cost by Type.
+        public double GetWardCostByType(WardType wardType)
+        {
+            double cost = 0;
+            foreach (var ward in Wards)
+            {
+                if (ward.WardType == wardType)
+                    cost += ward.GetWardCost();
+            }
+            return cost;
+        }
     }
 }
 
