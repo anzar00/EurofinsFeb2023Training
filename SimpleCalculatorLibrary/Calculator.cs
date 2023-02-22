e180c7cff69a40e0cca9ea33d95626d9bbb1b350
anzar000 <peerzada.anzar@gmail.com>
2023-02-22 14:11:23 +0530

    mock - library

--- SimpleCalculatorLibrary/Calculator.cs
+++ SimpleCalculatorLibrary/Calculator.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SimpleCalculator.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,12 +13,19 @@ namespace SimpleCalculatorLibrary
         // non negative numbers only
         // even numbers only
         // throws suitable expresion if business rule is violated
+        private ICalculatorRepo repo = null;
+        public Calculator(ICalculatorRepo repo)
+        { 
+            this.repo = repo;
+        }
         public int Sum(int a, int b)
         {
             if(a < 0 || b < 0)
                 throw new NegativeInputException("Negative numbers are not allowed");
             if(a % 2 != 0 || b % 2 != 0)
                 throw new OddInputException("Odd numbers are not allowed");
+            //CalculatorRepo repo = new CalculatorRepo();
+            repo.Save($"{a} + {b} = {a + b}");
             return a + b;
         }
     }
