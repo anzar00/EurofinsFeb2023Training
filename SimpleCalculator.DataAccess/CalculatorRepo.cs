703b9ed404dc8787bf7ffea687d9fc954c603f1a
anzar000 <peerzada.anzar@gmail.com>
2023-02-22 14:10:27 +0530

    mock - data access

---
+++ SimpleCalculator.DataAccess/CalculatorRepo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator.DataAccess
+{
+    public class CalculatorRepo : ICalculatorRepo
+    {
+        public bool Save(string data)
+        {
+            // logic to save data in to data store
+            File.WriteAllText("X:\\Training\\Day 13\\calculatorresult.txt", data);
+            return true;
+        }
+    }
+}
