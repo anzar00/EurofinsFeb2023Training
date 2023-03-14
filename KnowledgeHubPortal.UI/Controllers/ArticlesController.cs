878f4ccbd5ef19655bb3562bf233816be670fcd3
anzar000 <peerzada.anzar@gmail.com>
2023-03-14 16:59:52 +0530

    14/3/23 - logout

---
+++ KnowledgeHubPortal.UI/Controllers/ArticlesController.cs
@@ -0,0 +1,81 @@
+﻿using KnowledgeHubPortal.Data;
+using KnowledgeHubPortal.Domain;
+using KnowledgeHubPortal.Domain.Data;
+using KnowledgeHubPortal.Domain.Entities;
+using KnowledgeHubPortal.UI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KnowledgeHubPortal.UI.Controllers
+{
+    public class ArticlesController : Controller
+    {
+        // Do Not Do This - Inject with IOC instead
+
+        // GET: Articles
+
+        private IArticlesRepository Arepo = null;
+        private ICategoryRepository Crepo = null;
+        private IArticlesManager articlesManager = null;
+        private ICategoriesManager categoriesManager = null;
+
+        public ArticlesController()
+        {
+            Arepo = new ArticlesRepository();
+            Crepo = new CategoryRepository();
+            articlesManager = new ArticlesManager(Arepo);
+            categoriesManager = new CategoryManager(Crepo);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult SubmitArticle()
+        {
+            //var categories = categoriesManager.ListCategories();
+            var categories = from c in categoriesManager.ListCategories()
+                             select new SelectListItem
+                             {
+                                 Text = c.Name,
+                                 Value = c.CategoryId.ToString()
+                             };
+            ViewBag.CategoryId = categories;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SubmitArticle(ArticleSubmitViewModel articlevm)
+        {
+            // Server Side Validation
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("SubmitArticle");
+            }
+
+            // Convert ViewModel to DM 
+            Article article = new Article();
+            article.Title = articlevm.Title;
+            article.Description = articlevm.Description;
+            article.CategoryId = articlevm.CategoryId;
+            article.Url = articlevm.Url;
+
+            //Fill remaining properties
+            article.IsApproved = false;
+            article.DateSubmitted = DateTime.Now;
+            if (User.Identity.IsAuthenticated)
+                article.Submiter = User.Identity.Name;
+            else
+                article.Submiter = "Anonymous";
+
+            //Submit to domain
+            articlesManager.SubmitArticle(article);
+            TempData["Message"] = $"Article - {article.Title} added successfully for review...";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ KnowledgeHubPortal.UI/Models/ArticleViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KnowledgeHubPortal.UI.Models
+{
+    public class ArticleSubmitViewModel
+    {
+        [Required(ErrorMessage = "Enter article title.")]
+        [MinLength(10, ErrorMessage = "Title should be atleast 10 characters long.")]
+        [MaxLength(100)]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Enter article url.")]
+        [Url]
+        public string Url { get; set; }
+        [MaxLength(500)]
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
